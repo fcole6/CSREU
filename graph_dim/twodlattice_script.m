8b78cd790e659915ccbb6c56a35d8fabb4164b5a
julia <jfranck@ufl.edu>
2020-06-29 16:00:21 -0400

    scripts for data records

--- graph_dim/twodlattice_script.m
+++ graph_dim/twodlattice_script.m
@@ -1,39 +1,24 @@
 addpath('C:/Users/Franck/iCloudDrive/reu_2020/code/CSREU/pert_v_eval');
 addpath('C:/Users/Franck/iCloudDrive/reu_2020/code/CSREU/graph_structs');
+addpath('C:/Users/Franck/iCloudDrive/reu_2020/code/CSREU');
     N = 10000;
     A = twodlap(N);
     N = size(A,1);
     G = graph(A);
     L = -laplacian(G);
     
-    node = round((N/2)+floor(sqrt(N)/2));
+    v = get_data(A);
     
-    omeg = (.1: .02: .6)';
-    m = length(omeg);
-    lam = zeros(m,1);
+    node = round((N/2)+floor(sqrt(N)/2))
     
-    B = sparse(N,N);
-    B(node,node)=1;
-    
-    C = L + omeg(1)*B;
-    lam(1) = eigs(C,1,'largestreal');
-    
-    for j = 2:m
-        C = L + omeg(j)*B;
-        try
-        lam(j) = eigs(C,1,lam(j-1));
-        catch ME
-        lam(j) = eigs(C,1,'largestreal');
-        end
-    end
+    [lam,omeg,log_lam,log_omeg] = perturb4(A,node);
 
-    log_omeg = log(omeg);
-    log_lam = log(lam);
     p = polyfit(log_omeg,log_lam,1);
     y = polyval(p,log_omeg);
     
     p(1)
     
+    figure(2)
     plot(log_omeg,log_lam,log_omeg,y);
     title("Largest Eigenvalue v. Perturbation, 2-D Lattice");
     xlabel("log(omega)");
